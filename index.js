61088a029058105bbfc387b9146ab1429fee916a
Jemuel Elimanco <jemuelpup@gmail.com>
2021-06-17 16:31:13 +0800

    test function

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
-function sumArray(numberArray){
-    return numberArray.reduce((a,c)=>a+c,0);
-}
-
-module.exports.sumArray = sumArray;
+const sumArray = (numberArray) => numberArray.reduce((a,c)=>a+c,0);
+module.exports = {sumArray}
